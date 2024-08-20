f3c4eb7c62eaa6f31a42cbd5ecb116cc290fbb3e
anishadahal <anisha@teksewa.com>
2024-08-20 14:43:45 +0545

    Assignment 5 for file handling

---
+++ Assignment/Assignment5/Program.cs
@@ -0,0 +1,5 @@
+﻿﻿string filePath = @"C:\Users\anisha_teksewa\Desktop\dotnettraining\Assignment\Assignment5";
+List<Person> people = await CSVParser.ParseCSV(filePath);
+await PeopleReport.SaveMales(people, filePath);
+await PeopleReport.SaveFemales(people, filePath);
+await PeopleReport.SaveDotComUsers(people, filePath);
